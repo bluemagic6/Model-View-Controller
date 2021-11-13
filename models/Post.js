313326976b7e085dd4df0817fd6b6a0cbcd06c49
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-11-13 17:20:08 -0600

    updated post js

--- models/Post.js
+++ models/Post.js
@@ -1,16 +1,44 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
 Post.init(
     {
-        title: DataTypes.STRING,
-        body: DataTypes.STRING
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey:true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,        
+        },
+        content: {
+            type: DataTypes.STRING
+        },
+        date_create: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        }
     },
 
-    {sequelize}
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true, 
+        modelName: 'post',
+    }
 );
 
 module.exports = Post; 
