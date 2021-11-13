1a102fc01beed81064c93a8f23411a2e0231c610
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-11-13 17:21:53 -0600

    updated index js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const Post = require('./Post')
 const User = require('./User')
-const Comment = require('./Comment')
 
 
 User.hasMany(Post, {
@@ -12,13 +11,4 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-Post.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'user_id'
-})
-
-module.exports = { User, Post, Comment };
+module.exports = { User, Post };
